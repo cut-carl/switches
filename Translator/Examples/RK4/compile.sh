219b10890504e22889844c07086da35bb8fc56c8
Andreas Diavastos <diavastos@users.noreply.github.com>
2026-02-27 20:36:12 +0200

    Updated Examples and Benchmarks compile commands to the correct Switches parameters

--- Translator/Examples/RK4/compile.sh
+++ Translator/Examples/RK4/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Translate Application
-switches -s multicore -i rk4.c -t 4 -sched roundRobin -a compact -p screen
+switches -s multicore 4 4 -i rk4.c -t 4 -sched roundRobin -a compact -p screen
 
 # Compile Application
 make -f Makefile
